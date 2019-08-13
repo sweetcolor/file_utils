5315d5ceb372ff92518606ed8a055f0335a33dd8
Yurii Zhytskyi <sweet3color@gmail.com>
2019-08-13 07:51:03 +0300

    SC-002 init setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='smart-copy',
+    version='0.0.1',
+    description='Smart Copy Utility',
+    author='Yurii Zhytskyi',
+    author_email='',
+    url='',
+    packages=[],
+)
