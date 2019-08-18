32d94c81b1930dee7b56bc11b968ef9f0d3c360f
Yurii Zhytskyi <sweet3color@gmail.com>
2019-08-18 23:48:59 +0300

    SC-006 add Logs directory

--- logging.py
+++ logging.py
@@ -2,8 +2,11 @@ import pathlib
 
 
 class Logging:
+    LOG_PATH = 'log'
+
     def __init__(self, log_name: str):
-        self._log_file = open(log_name, 'w')
+        self._create_log_directory()
+        self._log_file = open(pathlib.Path(Logging.LOG_PATH, log_name), 'w')
 
     def __del__(self):
         self._log_file.close()
@@ -14,3 +17,12 @@ class Logging:
     @staticmethod
     def log_message(message: str, end: str = '\n') -> None:
         print(message, end=end)
+
+    @staticmethod
+    def _create_log_directory():
+        log_path = pathlib.Path(Logging.LOG_PATH)
+
+        if not log_path.exists():
+            if not log_path.is_dir():
+                raise Exception
+            log_path.mkdir()
